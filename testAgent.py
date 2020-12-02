d3f84ebfc83117f236233228c9c8b2535ff4110e
Krzysztof Paczuski <K.Paczuski@stud.elka.pw.edu.pl>
2020-12-02 16:59:57 +0100

    feat(): Adding test agent

---
+++ testAgent.py
@@ -0,0 +1,14 @@
+from spade import agent
+
+print('test')
+
+
+class DummyAgent(agent.Agent):
+    async def setup(self):
+        print("Hello World! I'm agent {}".format(str(self.jid)))
+
+
+dummy = DummyAgent("datacollector@localhost", "datacollector1")
+dummy.start()
+
+dummy.stop()
