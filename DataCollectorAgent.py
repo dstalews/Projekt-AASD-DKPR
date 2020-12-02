5d293907d541bcc3951362f29e70f37feab7e7b6
Krzysztof Paczuski <K.Paczuski@stud.elka.pw.edu.pl>
2020-12-02 18:32:52 +0100

    chore(): Added Agent specific logging

--- DataCollectorAgent.py
+++ DataCollectorAgent.py
@@ -17,32 +17,32 @@ class DataCollectorAgent(agent.Agent):
         data: Data
 
         async def run(self):
-            print("Starting collecting data")
-            print("Collecting data")
+            print("[DataCollector] Starting collecting data")
+            print("[DataCollector] Collecting data")
             await asyncio.sleep(5)
-            print("Data collected")
-            print("Preparing data to send")
+            print("[DataCollector] Data collected")
+            print("[DataCollector] Preparing data to send")
             local_data: Data = dict(
                 data_int=10
             )
             msg_to_send = Message(to="healthanalyzer@localhost")
             msg_to_send.body = dumps(local_data)
             await self.send(msg_to_send)
-            print("Message to Health Analyzer sent!")
+            print("[DataCollector] Message to Health Analyzer sent!")
             data = local_data
 
         async def on_start(self):
-            print("Starting collecting data Behaviour")
+            print("[DataCollector] Starting collecting data Behaviour")
 
         async def on_end(self):
-            print("Ended and now sending collected data")
+            print("[DataCollector] Ended and now sending collected data")
             await self.agent.stop()
 
     async def collect_data(self):
         pass
 
     async def setup(self):
-        print("Hello World! I'm agent {}".format(str(self.jid)))
+        print("[DataCollector] Hello World! I'm agent {}".format(str(self.jid)))
         start_at: datetime = datetime.now() + timedelta(seconds=5)
         collect_data_b = self.CollectData(period=20, start_at=start_at)
         self.add_behaviour(collect_data_b)
--- HealthAnalyzerAgent.py
+++ HealthAnalyzerAgent.py
@@ -5,16 +5,16 @@ from spade.behaviour import CyclicBehaviour
 class HealthAnalyzerAgent(agent.Agent):
     class RetrieveData(CyclicBehaviour):
         async def run(self):
-            print("Cyclic behaviour. I'm waiting for DataCollector's data")
+            print("[HealthAnalyzer] Cyclic behaviour. I'm waiting for DataCollector's data")
             msg = await self.receive(timeout=15)
 
             if msg:
-                print(f"Received data from DataCollector: {msg.body}")
+                print(f"[HealthAnalyzer] Received data from DataCollector: {msg.body}")
             else:
-                print("Didn't receive data from collector")
+                print("[HealthAnalyzer] Didn't receive data from collector")
                 # self.kill()
 
     async def setup(self):
-        print("Hello World! I'm agent {} I'm analyzing your health!".format(str(self.jid)))
+        print("[HealthAnalyzer] Hello World! I'm agent {} I'm analyzing your health!".format(str(self.jid)))
         retrieve_data_b = self.RetrieveData()
         self.add_behaviour(retrieve_data_b)
