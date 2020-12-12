a95a78cca59063a743108f2c51cd4af7e94483ca
Krzysztof Paczuski <K.Paczuski@stud.elka.pw.edu.pl>
2020-12-12 19:38:48 +0100

    feat(): Added requesting data from DataCollector after not receiving data for longer period of time

--- DataCollectorAgent.py
+++ DataCollectorAgent.py
@@ -11,6 +11,7 @@ from spade.template import Template
 
 class Data(TypedDict):
     data_int: int
+    datetime_created: datetime
 
 
 PERFORMED_ACTION_MESSAGE_TEMPLATE: Template = Template(
@@ -18,10 +19,15 @@ PERFORMED_ACTION_MESSAGE_TEMPLATE: Template = Template(
     sender="actionexecutor@localhost"
 )
 
+REQUEST_DATA_TEMPLATE: Template = Template(
+    metadata=dict(performative="request_data"),
+)
+
 
 class DataCollectorAgent(agent.Agent):
     performed_actions: List
     agent_name: str
+    data: Data
 
     class ReceivePerformedAction(CyclicBehaviour):
 
@@ -33,8 +39,19 @@ class DataCollectorAgent(agent.Agent):
                 self.agent.performed_actions.append(action)
                 print(f"[{self.agent.agent_name}] ActionExecutor sent message, {action}")
 
+    class DataRequest(CyclicBehaviour):
+        async def run(self):
+            msg = await self.receive(timeout=5)
+            if msg:
+                print(f"[{self.agent.agent_name}] Received data request from HealthAnalyzer")
+                msg_to_send = Message(
+                    "healthanalyzer@localhost",
+                    metadata=dict(performative="submit"),
+                )
+                msg_to_send.body = dumps(self.agent.data)
+                await self.send(msg_to_send)
+
     class CollectData(PeriodicBehaviour):
-        data: Data
 
         async def run(self):
             print(f"[{self.agent.agent_name}] Starting collecting data")
@@ -45,18 +62,11 @@ class DataCollectorAgent(agent.Agent):
             local_data: Data = dict(
                 data_int=10
             )
-            msg_to_send = Message(to="healthanalyzer@localhost")
+            msg_to_send = Message(to="healthanalyzer@localhost", metadata=dict(performative="submit"))
             msg_to_send.body = dumps(local_data)
+            self.agent.data = local_data
             await self.send(msg_to_send)
             print(f"[{self.agent.agent_name}] Message to Health Analyzer sent!")
-            self.data = local_data
-
-        async def on_start(self):
-            print(f"[{self.agent.agent_name}] Starting collecting data Behaviour")
-
-        async def on_end(self):
-            print(f"[{self.agent.agent_name}] Ended and now sending collected data")
-            await self.agent.stop()
 
     async def setup(self):
         self.agent_name = "DataCollector"
@@ -67,3 +77,4 @@ class DataCollectorAgent(agent.Agent):
         collect_data_b = self.CollectData(period=20, start_at=start_at)
         self.add_behaviour(collect_data_b)
         self.add_behaviour(self.ReceivePerformedAction(), template=PERFORMED_ACTION_MESSAGE_TEMPLATE)
+        self.add_behaviour(self.DataRequest(), template=REQUEST_DATA_TEMPLATE)
--- HealthAnalyzerAgent.py
+++ HealthAnalyzerAgent.py
@@ -1,12 +1,26 @@
 from json import dumps
 
 from spade import agent
-from spade.behaviour import CyclicBehaviour
+from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.message import Message
+from spade.template import Template
+
+DATA_COLLECTOR_DATA_TEMPLATE: Template = Template(
+    metadata=dict(performative='submit'),
+    sender='datacollector@localhost'
+)
 
 
 class HealthAnalyzerAgent(agent.Agent):
     agent_name: str
+
+    class RequestData(OneShotBehaviour):
+        async def run(self):
+            msg = Message(
+                "datacollector@localhost",
+                metadata=dict(performative="request_data")
+            )
+            await self.send(msg)
 
     class RetrieveData(CyclicBehaviour):
         async def run(self):
@@ -19,10 +33,12 @@ class HealthAnalyzerAgent(agent.Agent):
                 msg_to_send.body = dumps(dict(health_status="poor", blod_pressure="height"))
                 await self.send(msg_to_send)
             else:
-                print(f"[{self.agent.agent_name}] Didn't receive data from collector")
+                request_behaviour = self.agent.RequestData()
+                self.agent.add_behaviour(request_behaviour)
+                await request_behaviour.join()
 
     async def setup(self):
         self.agent_name = "HealthAnalyzer"
         print(f"[{self.agent_name}] Hello World! I'm agent {self.jid} I'm analyzing your health!")
         retrieve_data_b = self.RetrieveData()
-        self.add_behaviour(retrieve_data_b)
+        self.add_behaviour(retrieve_data_b, template=DATA_COLLECTOR_DATA_TEMPLATE)
